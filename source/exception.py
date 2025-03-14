faf8fb31d460b92979615087a00a098329d4321c
GinHikat <phamduyanh0816@gmail.com>
2025-03-14 11:03:37 +0700

    Input Logging/ Exception structure

---
+++ source/exception.py
@@ -0,0 +1,18 @@
+import sys
+import logging
+
+def error_message(error, detail:sys):
+    _,_,exc = detail.exc_info() #which file, which line the exception occurs
+    file_name = exc.tb_frame.f_code.co_filename
+    error_message = 'Error occured in {0} line {1} with message {2}'.format()
+    file_name, exc.tb_lineno, str(error)
+    
+    return error_message
+    
+class CustomException(Exception):
+    def __init__(self, message, detail:sys):
+        super().__init__(message)
+        self.message = error_message(error_message, detail = detail)
+        
+    def __str__(self):
+        return self.message
